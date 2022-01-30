4552e8a550fa2024f8824ed415c5ae11c4b7cbc0
soyboy <sbvegan@protonmail.com>
2022-01-29 18:22:00 -0800

    adding perpv2 contract dependencies

--- interfaces/IBaseToken.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: GPL-2.0-or-later
-pragma solidity 0.7.6;
-
-interface IBaseToken {
-    event PriceFeedChanged(address indexed priceFeed);
-
-    function getPriceFeed() external view returns (address);
-}
